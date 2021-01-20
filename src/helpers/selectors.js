14b9453974f511ad66c1ebe41b472b2db2e5efe8
Rene Wang <rene.wang1@hotmail.com>
2021-01-20 18:29:30 -0500

    implemented function getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export function getAppointmentsForDay(state, day) {
+  const appointments = [];
+  const dayFilter = state.days.filter(item => item.name === day)
+  const appointment_ids = dayFilter[0] ? dayFilter[0].appointments : []
+  for (const item of appointment_ids) {
+    for (const appointment in state.appointments) {
+      if (item === state.appointments[appointment].id) {
+        appointments.push(state.appointments[appointment])
+      }
+    }
+  }
+  return appointments;
+}
