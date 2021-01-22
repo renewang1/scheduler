4ee2a2b646341e066b809b10bfa4e742ad8651eb
Rene Wang <rene.wang1@hotmail.com>
2021-01-21 19:53:44 -0500

    added conditional to back function to check if history length was long enough to go back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -10,19 +10,10 @@ export default function useVisualMode(initialMode) {
   }
 
   const back = () => {
-    setMode(history[history.length - 2])
-    setHistory(history.slice(0, -1))
-    console.log(history)
-    // setHistory(() => {
-    //   console.log(history)
-    //   history = history.slice(0, -1)
-    // })
-    // setMode(history[history.length - 1])
-
-    // console.log(history)
-    // history = [...history.slice(0, -1)]
-
-    // console.log(history.slice(0, -1))
+    if (history.length > 1) {
+      setMode(history[history.length - 2])
+      setHistory(history.slice(0, -1))
+    }
   }
 
   return {mode, transition, back};
