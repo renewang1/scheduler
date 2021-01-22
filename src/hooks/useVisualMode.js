d7da9ed0dc6909fa53a1cafffc02b6ffa956a644
Rene Wang <rene.wang1@hotmail.com>
2021-01-21 20:56:23 -0500

    added default parameter to transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,7 +4,7 @@ export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode)
   const [history, setHistory] = useState([initialMode])
 
-  const transition = (newMode, replace) => {
+  const transition = (newMode, replace = false) => {
     if (replace) {
       setHistory(prev => [...prev.slice(0, -1), newMode])
     } else {
