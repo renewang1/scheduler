ce3a41b2eda00e48ae2e0aba532720b8799662eb
Rene Wang <rene.wang1@hotmail.com>
2021-01-26 17:07:26 -0500

    added promise chain to click Tuesday button and check expected outcome

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 describe("Application", () => {
-  
+
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
-    return waitForElement(() => getByText("Monday"));
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"))
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    })
   });
 })
