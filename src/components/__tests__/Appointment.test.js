3671c38e1c90b7ee3865cf4ec1113c719de6f8ec
Rene Wang <rene.wang1@hotmail.com>
2021-01-26 18:19:24 -0500

    removed renders without crashing tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -4,8 +4,4 @@ import { render } from "@testing-library/react";
 
 import Appointment from "components/Appointment";
 
-describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Appointment />);
-  });
-})
+describe("Appointment", () => {});
