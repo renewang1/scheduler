0d6d372bf1395cde617608e8b0b6fa685027937b
Rene Wang <rene.wang1@hotmail.com>
2021-01-26 22:39:35 -0500

    created 4 more test descriptions

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -50,4 +50,12 @@ describe("Application", () => {
     );
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, cancels an interview, and increases the spots remaining for Monday by 1", () => {});
+
+  it("loads data, edits an interview, and keeps the spots remaining for Monday the same", () => {});
+
+  it("shows the save error when failing to save an appointment", () => {});
+
+  it("shows the delete error when failing to delete an existing appointment", () => {}
 });
