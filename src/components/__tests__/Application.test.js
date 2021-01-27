ba9338013572526bc8ea88e028d20b8ca63c3c05
Rene Wang <rene.wang1@hotmail.com>
2021-01-26 23:22:21 -0500

    completed test to check if editing works

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,6 +11,7 @@ import {
   getByAltText,
   getByPlaceholderText,
   queryByText,
+  wait,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -80,7 +81,31 @@ describe("Application", () => {
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
-  it("loads data, edits an interview, and keeps the spots remaining for Monday the same", () => {});
+  it("loads data, edits an interview, and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application
+    const { container, debug, getByTestId } = render(<Application />);
+    // 2. Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // 3. Click the "Edit" button on the booked appointment
+    const appointment = getAllByTestId(
+      container,
+      "appointment"
+    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    fireEvent.click(getByAltText(appointment, "Edit"));
+    // 4. Check that the Form element is shown
+    expect(getByTestId("student-name-input")).toBeInTheDocument();
+    // 5. Edit the name for the interview
+    const input = getByPlaceholderText(appointment, "Enter Student Name");
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText(appointment, "Save"));
+    // 6. Wait until the element with the new name is displayed
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    // 7. Check that the DayListItem with the text "Monday" still has the text "1 spot remaining"
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
 
   it("shows the save error when failing to save an appointment", () => {});
 
